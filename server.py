4a108666b43101b7a8b06ddcfc2bbc27843eccbf
Dra7400 <88469667+Dra7400@users.noreply.github.com>
2022-05-26 14:29:24 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+import datetime
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", year=current_year)
+
+if __name__ == "__main__":
+    app.run(debug=True)
